95e27e6da3c70b2050e4fd920ccce07dff936c76
xhostcom <xhostcom@gmail.com>
2022-03-29 23:10:56 +0100

    Gitacora saved 2 changed files

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ApplicationRecord
   has_one_attached :avatar
   has_person_name
 
+  # Notifications & Services
   has_many :notifications, as: :recipient
   has_many :services
+  # has_many :members
 end
